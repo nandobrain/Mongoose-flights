34d0a4a467e25dd8c3f30771fe531040a3dc3d9d
nandobrain <prf.arturo@gmail.com>
2023-01-22 11:26:00 -0800

    almost...

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -3,25 +3,37 @@ const Flight = require('../models/flight')
 
 module.exports = {
     new: newTicket,
-    create
+    create,
+    addTicket
     
 
 }
 
 function newTicket(req, res) {
+    Ticket.find({})
+    .sort('seat')
+    .exec(function (err, tickets) {
     res.render('tickets/new', {
         title: 'add-ticket',
         tickets
     })
+})
 }
 
 
 
-function create(req, res) {
+function addTicket(req, res) {
     Ticket.findById(req.params.id, function(err, flight) {
         ticket.push(req.body.ticketsId)
         ticket.save(function(err) {
             res.redirect(`/flights/${flight._id}`)
         })
+    })
+}
+
+function create(req, res) {
+    req.body.date += 'T00:00'
+    Ticket.create(req.body, function(err, ticket) {
+    res.redirect('/tickets/new')
     })
 }
--- routes/tickets.js
+++ routes/tickets.js
@@ -5,6 +5,7 @@ const ticketsCtrl = require('../controllers/tickets')
 
 router.get('/tickets/new', ticketsCtrl.new)
 router.post('/tickets', ticketsCtrl.create)
-// router.post('/flights/:id/tickets', ticketsCtrl.addToFlight)
+router.post('/flights/:id/tickets', ticketsCtrl.addTicket)
+
 
 module.exports = router
